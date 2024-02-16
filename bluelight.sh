0a80e17e50ae995764490c8d79d435b8648e2f2b
dogette <77580077+dogette@users.noreply.github.com>
2024-02-16 14:48:30 -0700

    Add files via upload

--- bluelight.sh
+++ bluelight.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
+output=HDMI-0
 current_time=$(date +"%H%M")
 if ((1800<=$current_time && $current_time<=2400)) || (($current_time>=0000 && $current_time<=0600)); then
-	xrandr --output HDMI-0 --brightness 0.1
-	redshift -O 1000
+	xrandr --output $output --brightness 0.4 --gamma 1:0.2:0.2
 else
-	redshift -x	
+	xrandr --output $output --brightness 1 --gamma 1:1:1	
 fi
 
 
