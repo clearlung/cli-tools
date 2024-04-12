4888e8506d11d6406e1e853aa52722cb929478ab
clear <77580077+dogette@users.noreply.github.com>
2024-04-12 13:33:52 -0600

    Add files via upload

--- read.c
+++ read.c
@@ -4,15 +4,13 @@
 int main(void) {
 	FILE *fptr;
 	fptr = fopen("todo.txt", "r");
-	
-	if (ftpr != NULL) {
+	char readFile;
+
+	if (fptr != NULL) {
 		char readFile[100];
 		while(fgets(readFile, 100, fptr)) {
 			printf("%s", readFile);
 		}
-	} else {
-		printf("Unable to open file");
 	}
-
 	fclose(fptr);
 }
