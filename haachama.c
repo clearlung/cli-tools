9b0b01a40d92f8b271cd4d5d0369c7ffb6df1461
clear <77580077+dogette@users.noreply.github.com>
2024-04-22 13:37:37 -0600

    Add files via upload

--- haachama.c
+++ haachama.c
@@ -2,21 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int chama, counter = 0;
 	if (argc == 2) { 
 		char *a = argv[1];
-		int chama = atoi(a);
-		while (counter < chama) {
+		unsigned int chama = atoi(a);
+		for (int i = 0; i < chama; i++) {
 			printf("chama");
-			counter++;
 		}
-	} else {
-		printf("number of chamas: ");
-		scanf("%d", &chama);
-		while (counter < chama) {
-			printf("chama");
-			counter++;
-		}
-	}
-	printf("burger\n");
+		printf("burger\n");
+		return 0;
+	} else printf("value not provided\n");
+	return 0;
 }
