b03e6fc0de4afbf6881c42268801744c304c0fa2
clear <77580077+clearlung@users.noreply.github.com>
2024-06-30 15:37:51 -0600

    Add files via upload

--- pong.c
+++ pong.c
@@ -1,6 +1,8 @@
 #include "SDL2/SDL.h"
 #include <stdio.h>
 #include <time.h>
+#define WINDOW_WIDTH  (640)
+#define WINDOW_HEIGHT (480)
 
 typedef struct {
   float y;
@@ -57,12 +59,19 @@ int processEvents(SDL_Window *window, GameState *game) {
               game->ball.dy = (rnd > RAND_MAX/2) ? 3 : -3;
             }
           break;
+          case SDLK_f:
+            if (SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN_DESKTOP) {
+              SDL_SetWindowFullscreen(window, 0);
+            } else {
+              SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+            }
         } break;
       case SDL_QUIT:
         done = 1;
       break;
     }
   }
+
   const Uint8 *state = SDL_GetKeyboardState(NULL);
   if(state[SDL_SCANCODE_UP] && game->player.y > 0) {
     game->player.y -= 5;
@@ -77,9 +86,9 @@ void doRender(SDL_Renderer *renderer, GameState *game) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  
-  SDL_Rect playerPaddle = { 20, game->player.y, 4, 48 };
+  SDL_Rect playerPaddle = { 0 + 20, game->player.y, 4, 48 };
   SDL_RenderFillRect(renderer, &playerPaddle);
-  SDL_Rect CPUPaddle = { 620, game->cpu.y, 4, 48 };
+  SDL_Rect CPUPaddle = { WINDOW_WIDTH - 20, game->cpu.y, 4, 48 };
   SDL_RenderFillRect(renderer, &CPUPaddle);
   SDL_Rect ball = { game->ball.x, game->ball.y, 4, 4};
   SDL_RenderFillRect(renderer, &ball);
@@ -96,8 +105,8 @@ void CPUmovement(GameState *game) {
 }
 
 void reset(GameState *game) {
-  game->ball.x = 640/2;
-  game->ball.y = 480/2;
+  game->ball.x = WINDOW_WIDTH/2;
+  game->ball.y = WINDOW_HEIGHT/2;
   game->ball.dx = 0;
   game->ball.dy = 0;
   printf("p1: %i\np2: %i\n", game->points.p1, game->points.p2);
@@ -111,13 +120,12 @@ void collision(GameState *game) {
   if(ball->x <= 0) {
     game->points.p2 += 1;
     reset(game);
-  } else if(ball->x >= 640 - 4) {
+  } else if(ball->x >= WINDOW_WIDTH - 4) {
     game->points.p1 += 1;
     reset(game);
-  }
-  if(ball->x >= 620 && ball->x <= 620+4 && ball->y >= cpu->y && ball->y <= cpu->y + 48) {
+  } else if(ball->x >= WINDOW_WIDTH - 20 && ball->x <= WINDOW_WIDTH+4 && ball->y >= cpu->y && ball->y <= cpu->y + 48) {
     ball->dx *= -1;
-  } else if(ball->x <= 20 + 4 && ball->x >= 20 && ball->y >= player->y && ball->y <= player->y +48) {
+  } else if(ball->x <= 20 + 4 && ball->x >= 20 && ball->y >= player->y && ball->y <= player->y + 48) {
     ball->dx *= -1;
   } 
   ball->y += ball->dy;
@@ -126,7 +134,7 @@ void collision(GameState *game) {
   }
 }
 
-int main(int argc, char *argv[]) {
+int main(void) {
   GameState gameState;
   SDL_Window *window = NULL;
   SDL_Renderer *renderer = NULL;
@@ -134,12 +142,13 @@ int main(int argc, char *argv[]) {
   window = SDL_CreateWindow("pong",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
-      640,
-      480,
-      0
+      WINDOW_WIDTH,
+      WINDOW_HEIGHT,
+      SDL_WINDOW_RESIZABLE
       );
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   gameState.renderer = renderer;
+  SDL_RenderSetLogicalSize(renderer, WINDOW_WIDTH, WINDOW_HEIGHT);
   loadGame(&gameState);
   int done = 0;
   while(!done) {
