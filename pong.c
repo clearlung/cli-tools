117f00f984df40e56d4e956fad21f81a66554f8e
clear <77580077+clearlung@users.noreply.github.com>
2024-06-30 12:39:27 -0600

    Add files via upload

--- pong.c
+++ pong.c
@@ -1,17 +1,36 @@
 #include "SDL2/SDL.h"
 #include <stdio.h>
+#include <time.h>
 
 typedef struct {
   float y;
 } Player;
 
 typedef struct {
+  float x, y, dx, dy;
+} Ball;
+
+typedef struct {
+  int p1, p2;
+} Points;
+
+typedef struct {
   Player player;
+  Player cpu;
+  Ball ball;
+  Points points;
   SDL_Renderer *renderer;
 } GameState;
 
 void loadGame(GameState *game) {  
   game->player.y = 240;
+  game->cpu.y = 240;
+  game->ball.x = 640/2;
+  game->ball.y = 480/2;
+  game->ball.dx = 0;
+  game->ball.dy = 0;
+  game->points.p1 = 0;
+  game->points.p2 = 0;
 } 
 
 int processEvents(SDL_Window *window, GameState *game) {
@@ -30,6 +49,14 @@ int processEvents(SDL_Window *window, GameState *game) {
           case SDLK_ESCAPE:
             done = 1;
           break;
+          case SDLK_SPACE:
+            srandom((int)time(NULL));
+            int rnd = random();
+            if(game->ball.dx == 0 && game->ball.dy == 0) {
+              game->ball.dx = (rnd > RAND_MAX/2) ? 3 : -3;
+              game->ball.dy = (rnd > RAND_MAX/2) ? 3 : -3;
+            }
+          break;
         } break;
       case SDL_QUIT:
         done = 1;
@@ -37,10 +64,10 @@ int processEvents(SDL_Window *window, GameState *game) {
     }
   }
   const Uint8 *state = SDL_GetKeyboardState(NULL);
-  if(state[SDL_SCANCODE_UP]) {
+  if(state[SDL_SCANCODE_UP] && game->player.y > 0) {
     game->player.y -= 5;
   }
-  else if(state[SDL_SCANCODE_DOWN]) {
+  else if(state[SDL_SCANCODE_DOWN] && game->player.y < 480 -48) {
     game->player.y += 5;
   }
   return done;
@@ -49,21 +76,62 @@ int processEvents(SDL_Window *window, GameState *game) {
 void doRender(SDL_Renderer *renderer, GameState *game) {
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  
   SDL_Rect playerPaddle = { 20, game->player.y, 4, 48 };
   SDL_RenderFillRect(renderer, &playerPaddle);
-  SDL_Rect CPUPaddle = { 620, game->player.y, 4, 48 };
+  SDL_Rect CPUPaddle = { 620, game->cpu.y, 4, 48 };
   SDL_RenderFillRect(renderer, &CPUPaddle);
+  SDL_Rect ball = { game->ball.x, game->ball.y, 4, 4};
+  SDL_RenderFillRect(renderer, &ball);
   SDL_RenderPresent(renderer);
 }
- 
+
+void CPUmovement(GameState *game) {
+  while(game->cpu.y + 24 < game->ball.y && game->cpu.y < 480 - 48) {
+      game->cpu.y += 1;
+    }  
+  while(game->cpu.y + 24 > game->ball.y && game->cpu.y > 0) {
+      game->cpu.y -= 1;
+  } 
+}
+
+void reset(GameState *game) {
+  game->ball.x = 640/2;
+  game->ball.y = 480/2;
+  game->ball.dx = 0;
+  game->ball.dy = 0;
+  printf("p1: %i\np2: %i\n", game->points.p1, game->points.p2);
+}
+
+void collision(GameState *game) {
+  Ball *ball = &game->ball;
+  Player *player = &game->player;
+  Player *cpu = &game->cpu;
+  ball->x += ball->dx;
+  if(ball->x <= 0) {
+    game->points.p2 += 1;
+    reset(game);
+  } else if(ball->x >= 640 - 4) {
+    game->points.p1 += 1;
+    reset(game);
+  }
+  if(ball->x >= 620 && ball->x <= 620+4 && ball->y >= cpu->y && ball->y <= cpu->y + 48) {
+    ball->dx *= -1;
+  } else if(ball->x <= 20 + 4 && ball->x >= 20 && ball->y >= player->y && ball->y <= player->y +48) {
+    ball->dx *= -1;
+  } 
+  ball->y += ball->dy;
+  if(ball->y < 0 || ball->y > 480) {
+    ball->dy *= -1;
+  }
+}
+
 int main(int argc, char *argv[]) {
   GameState gameState;
   SDL_Window *window = NULL;
   SDL_Renderer *renderer = NULL;
   SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("Game Window",
+  window = SDL_CreateWindow("pong",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
       640,
@@ -72,11 +140,12 @@ int main(int argc, char *argv[]) {
       );
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   gameState.renderer = renderer;
-
   loadGame(&gameState);
   int done = 0;
   while(!done) {
     done = processEvents(window, &gameState);
+    CPUmovement(&gameState);
+    collision(&gameState);
     doRender(renderer, &gameState);
   }
   SDL_DestroyWindow(window);
