7d0e47f25ee06adbab94e66c21d9c596c157c25e
clear <77580077+clearlung@users.noreply.github.com>
2024-07-10 22:43:15 -0600

    Add files via upload

--- todo.c
+++ todo.c
@@ -2,60 +2,72 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-const char *path="/home/clear/projects/todo.txt";
+const char *path="/home/clear/todo/todo.txt";
+
+void read(FILE *fptr) {
+	fptr = fopen(path, "r");
+  char readFile;
+  if (fptr != NULL) {
+    char readFile[100];
+    while(fgets(readFile, 100, fptr)) {
+      printf("%s", readFile);
+    }
+  }
+  printf("\n");
+  fclose(fptr);
+}
+
+void deleteLine(FILE *fptr1, FILE *fptr2) {
+  int deleteLine, counter = 1;
+  char ch;
+  fptr1 = fopen(path, "r");	
+  fptr2 = fopen("todo.new", "w");
+  printf("line to delete: ");
+  scanf("%d", &deleteLine);
+  while (ch != EOF) {
+    ch = getc(fptr1);
+    if (counter != deleteLine) {
+      putc(ch, fptr2);
+    }
+    if (ch == '\n') {
+      counter++;
+    }      
+  }
+  fclose(fptr1);
+  fclose(fptr2);
+  remove(path);
+  rename("todo.new", path);
+}
+
+void appendLine(FILE *fptr, int argc, char **argv) {
+  fptr = fopen(path, "a");
+  if(argc == 3) {
+    fprintf(fptr, "%s\n", argv[2]);
+    fclose(fptr);
+  } else if(argc == 2) {
+    char task[30];
+    printf("name of task: ");
+    fgets(task, sizeof(task), stdin);
+    fprintf(fptr, "%s\n", task);
+    fclose(fptr);
+  }
+}
 
 int main(int argc, char **argv) {
 	int optionVal = 0;
 	FILE *fptr;
-	while(optionVal = getopt(argc, argv, "lrnt")) {
+	while(optionVal = getopt(argc, argv, "lrn")) {
 		switch(optionVal) {
 			case 'l':
-				fptr = fopen(path, "r");
-				char readFile;
-				if (fptr != NULL) {
-					char readFile[100];
-					while(fgets(readFile, 100, fptr)) {
-						printf("%s", readFile);
-					}
-				}
-				printf("\n");
-				fclose(fptr);
-				break;
+        read(fptr);
+        break;
 			case 'r':
 				FILE *fptr1, *fptr2;
-				int deleteLine, counter = 1;
-				char ch;
-				fptr1 = fopen(path, "r");	
-				fptr2 = fopen("todo.new", "w");
-				printf("line to delete: ");
-				scanf("%d", &deleteLine);
-				while (ch != EOF) {
-					ch = getc(fptr1);
-					if (counter != deleteLine) {
-						putc(ch, fptr2);
-					}
-					if (ch == '\n') {
-						counter++;
-					}      
-				}
-				fclose(fptr1);
-				fclose(fptr2);
-				remove(path);
-				rename("todo.new", path);
+        deleteLine(fptr1, fptr2);
 				break;
 			case 'n':
-				fptr = fopen(path, "a");
-				if(argc == 3) {
-					fprintf(fptr, "%s", argv[2]);
-					fclose(fptr);
-				} else if(argc == 2) {
-					char task[30];
-					printf("name of task: ");
-					fgets(task, sizeof(task), stdin);
-					fprintf(fptr, "%s", task);
-					fclose(fptr);
-				}
-				break;
+        appendLine(fptr, argc, argv);
+        break;
 			default:
 				return 0;
 		}
