42f1089af2354ce10ceb1a3b43b5eee8685fa01d
clear <77580077+clearlung@users.noreply.github.com>
2024-08-03 20:59:51 -0600

    modified to work with getchar. still broken.

--- replace.c
+++ replace.c
@@ -1,88 +1,91 @@
 //program instructions: replace occurences of searchTerm with replaceTerm
-//todo: make program read input through getchar insted of getc. also remove instances of the charactercount function because it's useless.
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdio.h>
 #define IN 1
 #define OUT 0
+#define EXIT 3
 
-char * copy(char *array, char *text);
-int find(char *text, char *search, int max);
-void replace(char *text, char *search, char *replace, int max);
+char searchTerm[] = "berry";
+char replaceTerm[] = "pineapple"; 
+int searchTermLength = sizeof(searchTerm) / sizeof(searchTerm[0]);
+int replaceTermLength = sizeof(replaceTerm) / sizeof(replaceTerm[0]);
+
+void copy(char *array);
+int find(char *text, char *search);
+void replace(char *text, char *search, char *replace);
 
 int main(void) {
-  char searchTerm[] = "berry";
-  char replaceTerm[] = "pineapple"; 
-  int searchTermLength = sizeof(searchTerm) / sizeof(searchTerm[0]);
-  int replaceTermLength = sizeof(replaceTerm) / sizeof(replaceTerm[0]);
   char fileContents[1000];
 
-  copy(fptr, fileContents, max);
-  replace(fileContents, searchTerm, replaceTerm, max);
+  copy(fileContents);
+  replace(fileContents, searchTerm, replaceTerm);
 }
 
-
-char * copy(char *array, int max) {
-  int i, c;  
-  i = 0;
-
-  while((c = getchar) != EOF && i<max;) {
-    c = getc(fptr); 
-    array[i++] = c;    
-  }
-
-  return text;
-}
-
-int find(char *text, char *search, int max) {
+int find(char *text, char *search) {
   int offset, i;
-
-  for (i=0;i<max;i++) {
+  i = 0;
+  while (text[i] != '\0') {
     if (text[i] == search[0]) {
       offset = i;
-      while (text[i] == search[i - offset] && text[i] != '\0') {
+      while (text[i] == search[i - offset] && text[i] != '\0')
         i++;
-      }
       if (i - offset == searchTermLength - 1)
         return offset;
     }
+    i++;
   }
-  printf("Match Not found\n");
+  
   return -1;
 }
 
 void replace(char *array, char *search, char *replace) {
-  int offset = find(text, search, max);
-  int i, j, k; // replace this method using state = IN/OUT for better functionality
-  int state;
-
-  if (offset != -1) {
-    for (i=0; i<offset; i++) {
-      if (text[i] != '\0')
-        putc(text[i], fptr2);    
+  char fileContentsNew[1000]; // new array...
+  int offset = find(array, search);
+  int i, j, state;
+  i = 0; j = 0; state = OUT;
+  
+  while (state != EXIT) {
+    if (state == OUT) { 
+      if (offset >= 0)
+        state = IN;      
+      else if (offset == -1) {
+        /* this fucks up atm. non priority
+         * while (fileContentsNew[j] != '\0')
+          putchar(fileContentsNew[j++]);
+        while (array[i] != '\0')
+          putchar(array[i++]);
+        */ 
+        state = EXIT;
+      }
     }
-    for (j=0; j < replaceTermLength; j++) {
-      if (replace[j] != '\0')
-        putc(replace[j], fptr2);
+    if (state == IN) {
+      for (i=0; i<offset; i++) {
+        if (array[i] != '\0')
+          putchar(array[i]);
+      }
+      for (i=0; i<replaceTermLength; i++) {
+        if (replace[i] != '\0')
+          putchar(replace[i]);
+      }
+      for (i=offset+searchTermLength-1; array[i] != '\0'; i++) {
+        fileContentsNew[j++] = array[i];  //problem with this is that array will not change. I will need a separate function to make this work.
+      }
+      
+      fileContentsNew[j] = '\0';
+      offset = find(fileContentsNew, search);
+      state = OUT;
     }
-    for (k = offset + searchTermLength - 1; text[k] != '\0'; k++) {
-      if(text[k] != '\0')
-        putc(text[k], fptr2);
-    }
-    remove(path);
-    rename(path2, path);
   }
 }
 
-/* useless function
-int characterCount(char *array) { 
-  int c, counter;
-  counter = 0;
 
-  while((c=getc(fptr)) c != EOF) {
-    counter += 1;
+void copy(char *array) {
+  int i, c;  
+  i = 0;
+
+  while((c = getchar()) != EOF) { 
+    array[i++] = c;    
   }
-
-  return counter;
+  array[i] = '\0';
 }
-*/
